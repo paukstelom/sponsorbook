3083abcea6d0cf636665a117b2992b735717ee0e
Edvin Sinkevič <55150562+edsinkevic@users.noreply.github.com>
2023-04-02 21:44:46 +0300

    Add dummy code (#1)

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from domain.models import Organization
 
 
 app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello, world"
+    org = Organization("Edvin", "2023-05-01")
+    return str(org)
