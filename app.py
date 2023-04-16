3eaa5af7d9d4163591883cf84438301ec3c9bb9c
edsinkevic <sinkevicedwin@gmail.com>
2023-04-16 12:43:15 +0300

    Refactor a bit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 db = MongoClient()['sponsorbook']
 
-tickets = {}
+tickets = db['tickets']
 
 
 @dataclass
@@ -28,26 +28,28 @@ def create_ticket():
     try:
         ticket = Ticket(id=str(gen_mongo_id()), title=response['title'],
                         description=response['description'])
-    except:
+    except KeyError:
         return 'Missing fields', 400
-    db['tickets'].insert_one({'_id': ObjectId(ticket.id),
-                              'title': ticket.title,
-                              'description:': ticket.description})
+
+    tickets.insert_one({'_id': ObjectId(ticket.id),
+                        'title': ticket.title,
+                        'description:': ticket.description})
     return ticket.__dict__, 201
 
 
 @app.route('/ticket/<id>', methods=['GET'])
-def return_ticket(id):
-    try:
-        ticket = db['tickets'].find_one({'_id': ObjectId(id)}, {"_id": False})
-    except:
-        return 'Ticket not found', 404
+def get_ticket(id: str):
+    ticket = tickets.find_one({'_id': ObjectId(id)}, {"_id": False})
+
+    if ticket is None:
+        return "Ticket not found", 404
+
     return ticket, 200
 
 
 @app.route('/tickets', methods=['GET'])
-def return_all_tickets():
-    return list(db['tickets'].find({}, {"_id": False})), 200
+def list_tickets():
+    return list(tickets.find({}, {"_id": False})), 200
 
 
 # Running flask
