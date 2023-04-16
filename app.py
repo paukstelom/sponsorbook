bd4903f6321b27dfcd67407020a4b07510225812
edsinkevic <sinkevicedwin@gmail.com>
2023-04-16 12:38:55 +0300

    Format and remove comments

--- app.py
+++ app.py
@@ -17,8 +17,10 @@ class Ticket:
     title: str
     description: str
 
+
 def gen_mongo_id():
     return ObjectId.from_datetime(datetime.now())
+
 
 @app.route('/ticket', methods=['POST'])
 def create_ticket():
@@ -28,17 +30,15 @@ def create_ticket():
                         description=response['description'])
     except:
         return 'Missing fields', 400
-    # tickets[ticket.id] = ticket
-    result = db['tickets'].insert_one({'_id': ObjectId(ticket.id),
-                                       'title': ticket.title,
-                                       'description:': ticket.description})
+    db['tickets'].insert_one({'_id': ObjectId(ticket.id),
+                              'title': ticket.title,
+                              'description:': ticket.description})
     return ticket.__dict__, 201
 
 
 @app.route('/ticket/<id>', methods=['GET'])
 def return_ticket(id):
     try:
-        # ticket = tickets[id]
         ticket = db['tickets'].find_one({'_id': ObjectId(id)}, {"_id": False})
     except:
         return 'Ticket not found', 404
