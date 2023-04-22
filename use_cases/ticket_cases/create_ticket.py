9b14f7bf33c66db385caab845d285684c59cb075
paukstello <paukstelom@gmail.com>
2023-04-22 13:25:16 +0300

    Connect ticket and sponsor cases

--- use_cases/ticket_cases/create_ticket.py
+++ use_cases/ticket_cases/create_ticket.py
@@ -1,11 +1,19 @@
 from fastapi.encoders import jsonable_encoder
 from motor.motor_asyncio import AsyncIOMotorCollection
 
+from models.errors import SponsorNotFound
 from models.py_object_id import PyObjectId
 from models.ticket import CreateTicketModel, Ticket
 
 
-async def create_ticket(tickets: AsyncIOMotorCollection, data: CreateTicketModel) -> Ticket:
+async def create_ticket(
+        tickets: AsyncIOMotorCollection,
+        sponsors: AsyncIOMotorCollection,
+        data: CreateTicketModel) -> Ticket | None:
+    res = await sponsors.find_one({'_id': data.sponsor_id})
+    if res is None:
+        raise SponsorNotFound
+
     ticket = Ticket(title=data.title, description=data.description, sponsor_id=PyObjectId(data.sponsor_id))
 
     await tickets.insert_one(jsonable_encoder(ticket))
