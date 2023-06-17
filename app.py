2c1174252a3bd1992fc39d8cca4f86aae3f1c7cc
edsinkevic <sinkevicedwin@gmail.com>
2023-06-17 14:32:15 +0300

    Lmao

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from models.authentication_models import Credentials
 from models.errors import InvalidCredentials
-from routers import sponsors, tickets
+from routers import sponsors, tickets, events
 from use_cases.authentication.login import authenticate_user
 
 app = FastAPI()
@@ -29,6 +29,7 @@ db = client['sponsorbook']
 
 app.include_router(sponsors.router)
 app.include_router(tickets.router)
+app.include_router(events.router)
 
 
 @app.post('/login', response_description='Login', response_model=str)
