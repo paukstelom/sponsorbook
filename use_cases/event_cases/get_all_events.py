78cbf5a07cb0cfda7205e69e142846218899b16f
edsinkevic <sinkevicedwin@gmail.com>
2023-06-18 11:24:49 +0300

    Lmao

--- use_cases/event_cases/get_all_events.py
+++ use_cases/event_cases/get_all_events.py
@@ -8,5 +8,5 @@ from models.event_models import Event
 async def get_events(
     database: AsyncIOMotorDatabase, page_size: int = 100
 ) -> AsyncGenerator[None, Event]:
-    for event in await database.events.find().to_list(page_size):
+    for event in await database.events.find({"is_archived": False}).to_list(page_size):
         yield Event.parse_obj(event)
