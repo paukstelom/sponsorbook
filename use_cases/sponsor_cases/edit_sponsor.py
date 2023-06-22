41e011fa3286d3499199e69ebe8fe5a995d57ef3
edsinkevic <sinkevicedwin@gmail.com>
2023-06-22 21:08:35 +0300

    Yes

--- use_cases/sponsor_cases/edit_sponsor.py
+++ use_cases/sponsor_cases/edit_sponsor.py
@@ -1,10 +1,36 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from models.errors import SponsorNotFound
-from models.sponsor_models import EditSponsorModel
+from models.sponsor_models import EditSponsorModel, Sponsor
+
+
 async def update_sponsor(database: AsyncIOMotorDatabase, sponsor_id: str, changes: EditSponsorModel) -> None:
-    new_values = {"$set": changes}
+    sponsor = await database.sponsors.find_one({"_id": sponsor_id})
+    sponsor = Sponsor.parse_obj(sponsor)
+
+    if changes.description is not None:
+        sponsor.description = changes.description
+
+    if changes.company_number is not None:
+        sponsor.company_number = changes.company_number
+
+    if changes.name is not None:
+        sponsor.name = changes.name
+
+    if changes.contacts is not None:
+        sponsor.contacts = changes.contacts
+
+    if changes.category is not None:
+        sponsor.category = changes.category
+
+    if changes.status is not None:
+        sponsor.status = changes.status
+
+    if changes.website is not None:
+        sponsor.website = changes.website
+
     res = await database.sponsors.replace_one(
-        {"_id": sponsor_id}, new_values)
+        {"_id": sponsor.id}, sponsor)
+
     if res.matched_count != 1:
         raise SponsorNotFound()
